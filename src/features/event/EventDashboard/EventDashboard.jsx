3a43bef7ffc4fcffc10ce8ecc92096cdaf7ff759
Jamal Bahammou <jamalbahammou456@gmail.com>
2019-04-25 00:08:49 +0200

    EOS 5

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -8,7 +8,7 @@ const eventsDashboard = [
    {
       id: '1',
       title: 'Trip to Tower of London',
-      date: '2019-03-27T11:00:00+00:00',
+      date: '2019-03-27',
       category: 'culture',
       description:
          'A software engineer develops various applications that enable users to accomplish tasks on their personal computers and electronic devices. Often, software engineers are employed by software publishers or computer systems design firms.',
@@ -32,7 +32,7 @@ const eventsDashboard = [
    {
       id: '2',
       title: 'Trip to Punch and Judy Pub',
-      date: '2019-04-24T03:47:00+01:00',
+      date: '2019-04-24',
       category: 'drinks',
       description:
          'A programmer, developer, coder, or software engineer is a person who creates computer software. The term computer programmer can refer to a specialist in one area of computers, or to a generalist who writes code for many kinds of software.',
@@ -59,11 +59,13 @@ const eventsDashboard = [
 class EventDashboard extends Component {
    state = {
       events: eventsDashboard,
-      isOpen: false
+      isOpen: false,
+      selectedEvent: null
    };
 
    handleFormOpen = () => {
       this.setState({
+         selectedEvent: null,
          isOpen: true
       });
    };
@@ -73,6 +75,27 @@ class EventDashboard extends Component {
          isOpen: false
       });
    };
+
+   handleUpdateEvent = (updatedEvent) => {
+      this.setState({
+         events: this.state.events.map(event => {
+            if (event.id === updatedEvent.id) {
+               return Object.assign({}, updatedEvent);
+            } else {
+               return event
+            }
+         }),
+         isOpen: false,
+         selectedEvent: null
+      })
+   }
+
+   handleOpenEvent = (eventToOpen) => () => {
+      this.setState({
+         selectedEvent: eventToOpen,
+         isOpen: true
+      })
+   }
 
    handleCreateEvent = (newEvent) => {
       newEvent.id = cuid();
@@ -84,15 +107,23 @@ class EventDashboard extends Component {
       })
    }
 
+   handleDeleteEvent = (eventId) => () => {
+      const updatedEvents = this.state.events.filter(e => e.id !== eventId);
+      this.setState({
+         events: updatedEvents
+      })
+   }
+
    render() {
+      const { selectedEvent } = this.state;
       return (
          <Grid>
             <Grid.Column width={10}>
-               <EventList events={this.state.events} />
+               <EventList deleteEvent={this.handleDeleteEvent} onEventOpen={this.handleOpenEvent} events={this.state.events} />
             </Grid.Column>
             <Grid.Column width={6}>
                <Button onClick={this.handleFormOpen} positive content="Create Event" />
-               {this.state.isOpen && <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />}
+               {this.state.isOpen && <EventForm updateEvent={this.handleUpdateEvent} selectedEvent={selectedEvent} createEvent={this.handleCreateEvent} handleCancel={this.handleCancel} />}
             </Grid.Column>
          </Grid>
       )
