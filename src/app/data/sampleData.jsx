9fdb17a481cadf75c1bc0a81f2f0fb356d465607
Jamal Bahammou <jamalbahammou456@gmail.com>
2019-05-08 15:27:44 +0000

    EOS 13

--- src/app/data/sampleData.jsx
+++ src/app/data/sampleData.jsx
@@ -3,7 +3,7 @@ const sampleData = {
       {
          id: '1',
          title: 'Trip to Tower of London',
-         date: '2019-03-27',
+         date: '2019-03-27T18:00:00',
          category: 'culture',
          description:
             'A software engineer develops various applications that enable users to accomplish tasks on their personal computers and electronic devices. Often, software engineers are employed by software publishers or computer systems design firms.',
@@ -31,7 +31,7 @@ const sampleData = {
       {
          id: '2',
          title: 'Trip to Punch and Judy Pub',
-         date: '2019-04-24',
+         date: '2019-04-24T18:00:59',
          category: 'drinks',
          description:
             'A programmer, developer, coder, or software engineer is a person who creates computer software. The term computer programmer can refer to a specialist in one area of computers, or to a generalist who writes code for many kinds of software.',
